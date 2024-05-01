1a183d4f8aadabf81f0472854a0896594a12df49
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2024-05-01 15:29:24 -0300

    Fixing 2022 questions

--- post_questao.py
+++ post_questao.py
@@ -15,24 +15,27 @@ def send(year):
         with open(year + '/' + json_path) as json_file:
             questao = json.load(json_file)
             questao_json = {
-                'area': questao['Componente'],
-                'subarea': questao['Subarea'],
                 'numero': int(questao["Numero"]),
                 'ano': int(questao['Ano']),
+                'area': questao['Componente'],
+                'subarea': questao['Subarea'],
                 'resposta': int(questao['Resposta']),
                 'alternativas': questao['Alternativas'],
+                'enunciado': questao['Enunciado'],
                 'imagens': {'enunciado': questao['Imagens'], 'alternativa_a': [], 'alternativa_b': [],
                             'alternativa_c': [], 'alternativa_d': [], 'alternativa_e': []},
-                'enunciado': questao['Enunciado']
             }
 
             r = requests.post(
                 'http://localhost:8060/questao/', json=questao_json)
             if r.status_code == 201:
                 print('year: ' + year + ', json_path: ' + json_path)
+            else:
+                print('year: ' + year + ', json_path: ' + json_path, r.content)
 
 
 send('2016')
 send('2017')
 send('2018')
 send('2019')
+send('2022')
